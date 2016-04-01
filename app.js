02e1f3dc3c5112ce21e539d8eb79e2f723b14416
Elad Katz <eladkt@gmail.com>
2016-04-01 03:13:44 +0300

    basic admin working

--- app.js
+++ app.js
@@ -3,8 +3,9 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
 app.use('/node_modules/ng-admin/build/', express.static(__dirname + '/node_modules/ng-admin/build/'));
+
+app.use(express.static('public'));
 
 
 app.listen(3000, function () {
--- public/admin.js
+++ public/admin.js
@@ -7,18 +7,101 @@ myApp.config(['NgAdminConfigurationProvider', function (nga) {
     var admin = nga.application('My First Admin')
         .baseApiUrl('http://localhost:6007/api/v1/');
 
-    var product = nga.entity('products').identifier(nga.field('_id'));
+    const LinkSchema = {
+        name: String,
+        productId: Number,
+        isActive: Boolean,
+        isDefault: Boolean,
+        url: String,
+        priceValue: Number,
+        priceCurrency: String,
+        oldPrice: Number,
+        priceText: String
+    };
+
+    var link = nga.entity('links').identifier(nga.field('id'));
+
+    link.listView().fields([
+        nga.field('name').isDetailLink(true),
+        nga.field('productId', 'number'),
+        nga.field('isActive', 'boolean'),
+        nga.field('isDefault', 'boolean'),
+        nga.field('url'),
+        nga.field('priceValue', 'number').format('$0,0.00'),
+        nga.field('priceCurrency', 'choice')
+            .choices([
+                {label: 'USD', value: 'USD'},
+                {label: 'EUR0', value: 'EURO'}
+            ]),
+        nga.field('oldPrice', 'number').format('$0,0.00'),
+        nga.field('priceText')
+    ]);
+
+    link.showView().fields(link.listView().fields());
+
+    link.creationView().fields(link.listView().fields());
+
+    link.editionView().fields(link.listView().fields());
+
+    admin.addEntity(link);
+
+
+    var partner = nga.entity('partners').identifier(nga.field('id'));
+
+    partner.listView().fields([
+        nga.field('name').isDetailLink(true),
+        nga.field('advertiserId', 'number'),
+        nga.field('email'),
+        nga.field('phone'),
+        nga.field('address'),
+        nga.field('website'),
+        nga.field('logo')
+    ]);
+
+    partner.showView().fields([
+        nga.field('name'),
+        nga.field('advertiserId', 'number'),
+        nga.field('email'),
+        nga.field('phone'),
+        nga.field('address'),
+        nga.field('website'),
+        nga.field('logo')
+    ]);
+
+    partner.creationView().fields([
+        nga.field('name')
+            .attributes({placeholder: 'name can only contain letters, numbers and dashes'})
+            .validation({required: true, pattern: '[a-z0-9\-]+'}),
+        nga.field('advertiserId', 'number')
+            .validation({required: true, pattern: '[a-z0-9\-]+'}),
+        nga.field('email', 'email'),
+        nga.field('phone'),
+        nga.field('address'),
+        nga.field('website'),
+        nga.field('logo')
+
+    ]);
+
+    partner.editionView().fields(partner.creationView().fields());
+
+    admin.addEntity(partner);
+
+
+    var product = nga.entity('products').identifier(nga.field('id'));
 
     product.listView().fields([
         nga.field('name').isDetailLink(true),
-        nga.field('productId'),
+        nga.field('productId', 'number'),
         nga.field('logoPath'),
         nga.field('bottomLine')
     ]);
 
     product.creationView().fields([
-        nga.field('name'),
-        nga.field('productId'),
+        nga.field('name')
+            .attributes({placeholder: 'name can only contain letters, numbers and dashes'})
+            .validation({required: true, pattern: '[a-z0-9\-]+'}),
+        nga.field('productId', 'number')
+            .validation({required: true, pattern: '[a-z0-9\-]+'}),
         nga.field('logoPath'),
         nga.field('bottomLine')
     ]);
@@ -27,7 +110,7 @@ myApp.config(['NgAdminConfigurationProvider', function (nga) {
 
     product.showView().fields([
         nga.field('name'),
-        nga.field('productId'),
+        nga.field('productId', 'number'),
         nga.field('logoPath'),
         nga.field('bottomLine')
     ]);
@@ -50,3 +133,24 @@ myApp.config(['NgAdminConfigurationProvider', function (nga) {
     // attach the admin application to the DOM and execute it
     nga.configure(admin);
 }]);
+
+
+myApp.config(function (RestangularProvider) {
+    RestangularProvider.addResponseInterceptor(function (data, operation, what, url, response) {
+        //if (operation == "getList") {
+        //response.totalCount = data.meta.pagination.total;
+        data = data.content;
+        //}
+        console.log(response);
+        return data;
+    });
+    //RestangularProvider.addFullRequestInterceptor(function(element, operation, what, url, headers, params, httpConfig) {
+    //    if (operation == 'getList' && what == 'entityName') {
+    //        params.page = params._page;
+    //        params.limit = params._perPage;
+    //        delete params._page;
+    //        delete params._perPage;
+    //    }
+    //    return { params: params };
+    //});
+});
